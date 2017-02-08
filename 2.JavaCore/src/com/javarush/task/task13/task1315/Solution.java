b9da89d61283c7a6433e29c5034e6c1ba2b8095c
bycter <bycter@gmail.com>
2017-02-08 21:57:18 +0300

    level13.task1315 completed

---
+++ 2.JavaCore/src/com/javarush/task/task13/task1315/Solution.java
@@ -0,0 +1,79 @@
+package com.javarush.task.task13.task1315;
+
+/* 
+Dog, Cat и Mouse
+1. Создай классы Dog, Cat и Mouse.
+2. Реализуй интерфейсы в добавленных классах, учитывая что:
+— Кот(Cat) может передвигаться, может кого-то съесть и может быть сам съеден.
+— Мышь(Mouse) может передвигаться и может быть съедена.
+— Собака(Dog) может передвигаться и съесть кого-то.
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+
+    //может двигаться
+    public interface Movable {
+
+        void move();
+    }
+
+    //может быть съеден
+    public interface Eatable {
+
+        void eaten();
+    }
+
+    //может кого-нибудь съесть
+    public interface Eat {
+
+        void eat();
+    }
+
+    public class Dog implements Movable, Eat {
+
+        @Override
+        public void eat() {
+
+        }
+
+        @Override
+        public void move() {
+
+        }
+    }
+
+    public class Cat implements Movable, Eatable, Eat {
+
+        @Override
+        public void eat() {
+
+        }
+
+        @Override
+        public void eaten() {
+
+        }
+
+        @Override
+        public void move() {
+
+        }
+    }
+
+    public class Mouse implements Movable, Eatable {
+
+        @Override
+        public void eaten() {
+
+        }
+
+        @Override
+        public void move() {
+
+        }
+    }
+}
