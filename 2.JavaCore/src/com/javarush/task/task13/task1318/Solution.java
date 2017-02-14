accf7bf2b1ae18d983a0882f72aefc072466eff3
bycter <bycter@gmail.com>
2017-02-14 23:47:08 +0300

    syntax.task13.task1318 completed

--- 2.JavaCore/src/com/javarush/task/task13/task1318/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task13/task1318/Solution.java
@@ -7,8 +7,23 @@ package com.javarush.task.task13.task1318;
 3. Не забыть освободить ресурсы. Закрыть поток чтения с файла и поток ввода с клавиатуры.
 */
 
-public class Solution {
-    public static void main(String[] args) {
+import java.io.*;
 
+public class Solution {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        String fileName = reader.readLine();
+
+        BufferedReader reader1 = new BufferedReader(new InputStreamReader(new FileInputStream(fileName)));
+
+        while (reader1.ready())
+        {
+            System.out.println(reader1.readLine()); //читаем один байт из потока для чтения
+        }
+
+        reader1.close();
+        reader.close();
     }
 }
