5282997db2f78fc2c2e14c8f5947f2522dd0ac66
bycter <bycter@gmail.com>
2017-03-14 18:39:29 +0300

    core.task14.task1409 completed

--- 2.JavaCore/src/com/javarush/task/task14/task1409/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task14/task1409/Solution.java
@@ -2,6 +2,13 @@ package com.javarush.task.task14.task1409;
 
 /* 
 Мосты
+1. Создать интерфейс Bridge с методом int getCarsCount().
+2. Создать классы WaterBridge и SuspensionBridge, которые реализуют интерфейс Bridge.
+3. Метод getCarsCount() должен возвращать любое фиксированное значение типа int.
+4. Метод getCarsCount() должен возвращать различные значения для различных классов.
+5. В классе Solution создать публичный метод println(Bridge bridge).
+6. В методе println вывести на консоль значение getCarsCount() для объекта bridge.
+7. Каждый класс и интерфейс должны быть в отдельных файлах.
 */
 
 public class Solution {
@@ -10,6 +17,9 @@ public class Solution {
         println(new SuspensionBridge());
     }
 
-    //add println method here
+    public static void println(Bridge bridge) {
+
+        System.out.println(bridge.getCarsCount());
+    }
 }
 
