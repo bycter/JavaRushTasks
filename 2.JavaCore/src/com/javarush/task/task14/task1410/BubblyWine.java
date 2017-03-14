36d0aac396e7ab8f4de30bed3765192c1dcb0f07
bycter <bycter@gmail.com>
2017-03-14 19:01:19 +0300

    core.task14.task1410 completed

---
+++ 2.JavaCore/src/com/javarush/task/task14/task1410/BubblyWine.java
@@ -0,0 +1,11 @@
+package com.javarush.task.task14.task1410;
+
+public class BubblyWine extends Wine {
+
+    @Override
+    public String getHolidayName() {
+
+        return "Новый Год";
+    }
+
+}
--- 2.JavaCore/src/com/javarush/task/task14/task1410/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task14/task1410/Solution.java
@@ -2,6 +2,11 @@ package com.javarush.task.task14.task1410;
 
 /* 
 Дегустация вин
+1. Создать абстрактный класс Drink с реализованным методом public void taste(), который выводит в консоль «Вкусно«.
+2. Создать класс Wine, который наследуется от Drink, с реализованным методом public String getHolidayName(), который возвращает строку «День Рождения«.
+3. Создать класс BubblyWine, который наследуется от Wine, с реализованным методом public String getHolidayName(), который возвращает строку «Новый Год«.
+4. Написать реализацию методов getDeliciousDrink, getWine, getBubblyWine.
+5. Все классы и интерфейс должны находиться в отдельных файлах.
 */
 
 public class Solution {
@@ -13,14 +18,15 @@ public class Solution {
     }
 
     public static Drink getDeliciousDrink() {
-
+        return new Wine();
     }
 
     public static Wine getWine() {
-
+        return new Wine();
     }
 
     public static Wine getBubblyWine() {
-
+        return new BubblyWine();
     }
 }
+
