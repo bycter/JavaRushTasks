f69188a341447fa4a6a1ea4d304a74c43f43cb55
bycter <bycter@gmail.com>
2017-03-15 21:56:26 +0300

    core.task14.task1418 completed

--- 2.JavaCore/src/com/javarush/task/task14/task1418/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task14/task1418/Solution.java
@@ -5,32 +5,56 @@ import java.util.List;
 
 /* 
 Исправь 4 ошибки
+1. Подумай что делает метод main.
+2. Создай в классе Solution статические методы initList(List<Number> list), printListValues(List<Number> list), processCastedObjects(List<Number> list).
+3. Найди блок кода которая заполняет значениями список и перемести его в метод initList.
+4. Найди блок кода который в цикле for выводит на экран содержимое списка и перемести его в метод printListValues.
+5. Найди блок кода в котором для каждого объекта списка проверяется тип и выводятся сообщения на экран и перемести его в метод processCastedObjects.
+6. Исправь 2 ошибки в методе printListValues так, чтобы на экран корректно выводилось содержимое переданного в качестве параметра списка.
+7. Исправь 2 ошибки в методе processCastedObjects связанные с приведением типов
+— для объекта типа Float нужно вывести «Is float value defined? » + !([Float_object].isNaN()).
+— для объекта типа Double нужно вывести «Is double value infinite? » + [Double_object].isInfinite().
 */
 
 public class Solution {
     public static void main(String[] args) {
-        List<Number> list = new LinkedList<Number>();
+        List<Number> list = new LinkedList<>();
         //3
+        initList(list);
+
+        //4 - Исправь 2 ошибки
+        printListValues(list);
+
+        //5
+        processCastedObjects(list);
+    }
+
+    private static void processCastedObjects(List<Number> list) {
+
+        for (Number object : list) {
+            //Исправь 2 ошибки
+            if (object instanceof Float) {
+                Float a = (Float) object;
+                System.out.println("Is float value defined? " + !(a.isNaN()));
+            } else if (object instanceof Double) {
+                Double a = (Double) object;
+                System.out.println("Is double value infinite? " + a.isInfinite());
+            }
+        }
+    }
+
+    private static void printListValues(List<Number> list) {
+
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+    }
+
+    private static void initList(List<Number> list) {
+
         list.add(new Double(1000f));
         list.add(new Double("123e-445632"));
         list.add(new Float(-90 / -3));
         list.remove(new Double("123e-445632"));
-
-        //4 - Исправь 2 ошибки
-        for (int i = 0; i <= list.size(); i--) {
-            System.out.println(list.get(i));
-        }
-
-        //5
-        for (Number object : list) {
-            //Исправь 2 ошибки
-            if (object instanceof Float) {
-                Double a = (Double) object;
-                System.out.println("Is float value defined? " + !(a.isNaN()));
-            } else if (object instanceof Double) {
-                Float a = (Float) object;
-                System.out.println("Is double value infinite? " + a.isInfinite());
-            }
-        }
     }
 }
