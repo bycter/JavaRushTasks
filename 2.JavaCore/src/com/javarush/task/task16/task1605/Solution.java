108cca2c9f3381dcb304446c2fab4276d5e104bb
Denis Yoshchyk <bycter@gmail.com>
2017-03-30 09:53:30 +0300

    core.task16.task1605 completed

--- 2.JavaCore/src/com/javarush/task/task16/task1605/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task16/task1605/Solution.java
@@ -28,21 +28,41 @@ public class Solution {
         Date stopPlaying();
     }
 
-    public static class Violin {
+    public static class Violin implements MusicalInstrument {
         private String owner;
 
         public Violin(String owner) {
             this.owner = owner;
         }
 
+        @Override
         public Date startPlaying() {
             System.out.println(this.owner + " starts playing");
             return new Date();
         }
 
+        @Override
         public Date stopPlaying() {
             System.out.println(this.owner + " stops playing");
             return new Date();
+        }
+/*
+2. Реализуй необходимый метод в нити Violin. Реализация должна быть следующей:
+2.1. Считай время начала игры — метод startPlaying().
+2.2. Подожди 1 секунду — метод sleepNSeconds(int n), где n — количество секунд.
+2.3. Считай время окончания игры — метод stopPlaying().
+2.4. Выведи на консоль продолжительность игры в миллисекундах.
+
+Пример «Playing 1002 ms«.
+ */
+        @Override
+        public void run() {
+
+            Date startTime = startPlaying();
+            sleepNSeconds(1);
+            Date endTime = stopPlaying();
+
+            System.out.println("Playing " + (endTime.getTime() - startTime.getTime()) + " ms");
         }
     }
 }
