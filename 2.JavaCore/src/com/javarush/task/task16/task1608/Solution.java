dc0315f6aba73b6239ed0f1ff8a126901cd9dfa9
bycter <bycter@gmail.com>
2017-04-01 15:55:47 +0300

    core.task16.task1608 completed

--- 2.JavaCore/src/com/javarush/task/task16/task1608/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task16/task1608/Solution.java
@@ -2,6 +2,17 @@ package com.javarush.task.task16.task1608;
 
 /* 
 Продвижение на политических дебатах
+1. Разберись, что делает программа.
+2. Нужно сделать так, чтобы Иванов сказал больше всего речей на политических дебатах.
+3. Подумай, какой метод можно вызвать у объекта ivanov, чтобы Иванов разговаривал, пока не завершится всё свободное время.
+
+
+Требования:
+1. Вызови метод join у нужного объекта.
+2. Метод toString класса политик Politician должен выводить сколько речей сказал политик, например: "Иванов сказал речь 35 раз".
+3. Программа должна создавать 3 объекта типа Politician.
+4. Методы, которые отвечают за вывод в консоль, не изменять.
+5. Вывод программы должен свидетельствовать о том, что Иванов сказал больше всего речей на политических дебатах.
 */
 
 public class Solution {
@@ -10,6 +21,7 @@ public class Solution {
 
     public static void main(String[] args) throws InterruptedException {
         Politician ivanov = new Politician("Иванов");
+        ivanov.join();
         Politician petrov = new Politician("Петров");
         Politician sidorov = new Politician("Сидоров");
 
@@ -29,6 +41,7 @@ public class Solution {
             start();
         }
 
+        @Override
         public void run() {
             while (countSounds < totalCountSpeeches * soundsInOneSpeech) {
                 countSounds++;
