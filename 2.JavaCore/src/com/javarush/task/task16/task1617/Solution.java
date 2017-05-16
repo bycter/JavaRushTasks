ec32c19a387b4dc81faf7da9b8babbdd6b7e2342
Denis Yoshchyk <bycter@gmail.com>
2017-05-16 12:18:25 +0300

    core.task16.task1617 completed Отсчет на гонках

--- 2.JavaCore/src/com/javarush/task/task16/task1617/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task16/task1617/Solution.java
@@ -2,23 +2,52 @@ package com.javarush.task.task16.task1617;
 
 /* 
 Отсчет на гонках
+1. Разберись, что делает программа.
+2. Реализуй логику метода run так, чтобы каждую секунду через пробел
+выдавался отсчет начиная с countSeconds до 1, а потом слово [Марш!] (см примеры).
+3. Если нить работает 3.5 секунды или более, прерви ее методом interrupt и внутри нити выведи в консоль слово [Прервано!].
+
+Пример для countSeconds=4 :
+«4 3 2 1 Прервано!»
+
+4. Если нить работает менее 3.5 секунд, она должна завершиться сама.
+Пример для countSeconds=3 :
+«3 2 1 Марш!»
+
+PS: метод sleep выбрасывает InterruptedException.
 */
 
 public class Solution {
-    public static volatile int countSeconds = 3;
+
+    public static volatile int countSeconds = 4;
 
     public static void main(String[] args) throws InterruptedException {
+
         RacingClock clock = new RacingClock();
-        //add your code here - добавь код тут
+        Thread.sleep(3500);
+        clock.interrupt();
+
     }
 
     public static class RacingClock extends Thread {
+
         public RacingClock() {
             start();
         }
 
+        @Override
         public void run() {
-            //add your code here - добавь код тут
+            while (countSeconds > 0) {
+                System.out.print(countSeconds + " ");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    System.out.println("Прервано!");
+                    return;
+                }
+                countSeconds--;
+            }
+            System.out.println("Марш!");
         }
     }
 }
