bf740ae117ad153e3a7d536efdf694a3688615ef
Denis Yoshchyk <bycter@gmail.com>
2017-05-16 10:03:18 +0300

    core.task16.task1616 completed Считаем секунды

--- 2.JavaCore/src/com/javarush/task/task16/task1616/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task16/task1616/Solution.java
@@ -6,10 +6,23 @@ import java.io.InputStreamReader;
 
 /* 
 Считаем секунды
+1. Напиши реализацию метода run в нити Stopwatch (секундомер).
+2. Stopwatch должен посчитать количество секунд, которое прошло от создания нити до ввода строки.
+3. Выведи количество секунд в консоль.
+
+
+Требования:
+1. Метод run класса Stopwatch (секундомер) должен содержать цикл.
+2. Метод run должен вызывать Thread.sleep(1000).
+3. Метод run должен увеличивать значение поля seconds на 1 каждую секунду.
+4. После прерывания работы нити Stopwatch (вызова метода interrupt), метод run должен вывести количество секунд (seconds) в консоль.
+5. В классе Stopwatch должен быть только один метод run.
 */
 
 public class Solution {
+
     public static void main(String[] args) throws IOException {
+
         InputStreamReader in = new InputStreamReader(System.in);
         BufferedReader reader = new BufferedReader(in);
         //create and run thread
@@ -24,11 +37,16 @@ public class Solution {
     }
 
     public static class Stopwatch extends Thread {
+
         private int seconds;
 
+        @Override
         public void run() {
             try {
-                //add your code here - добавьте код тут
+                while(!isInterrupted()) {
+                    seconds++;
+                    Thread.sleep(1000);
+                }
             } catch (InterruptedException e) {
                 System.out.println(seconds);
             }
