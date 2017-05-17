415945826da6c1a85070979eca3f015cc7aa9697
Denis Yoshchyk <bycter@gmail.com>
2017-05-17 12:02:48 +0300

    core.task16.task1619 completed А без interrupt слабо?

--- 2.JavaCore/src/com/javarush/task/task16/task1619/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task16/task1619/Solution.java
@@ -2,6 +2,18 @@ package com.javarush.task.task16.task1619;
 
 /* 
 А без interrupt слабо?
+Разберись, как работает программа.
+Сделай так, чтобы в методе ourInterruptMethod можно было сделать так, чтобы нить TestThread завершилась сама.
+Нельзя использовать метод interrupt.
+
+
+Требования:
+1. В классе Solution должен быть публичный статический метод ourInterruptMethod без параметров.
+2. Метод run должен выводить надпись "he-he" каждые пол секунды, пока не будет вызван метод ourInterruptMethod.
+3. Необходимо изменить условие цикла while в методе run.
+4. Метод main должен создавать объект типа Thread передавая в конструктор объект типа TestThread.
+5. Метод main должен вызывать метод start у объекта типа Thread.
+6. Метод main должен вызывать метод ourInterruptMethod.
 */
 
 public class Solution {
@@ -13,12 +25,15 @@ public class Solution {
     }
 
     public static void ourInterruptMethod() {
-
+        isCancel = true;
     }
 
+    public static volatile boolean isCancel = false;
+
     public static class TestThread implements Runnable {
+        @Override
         public void run() {
-            while (true) {
+            while (!isCancel) {
                 try {
                     System.out.println("he-he");
                     Thread.sleep(500);
