baa183ff70a4c294415e7e3b5e6413fab3cafad0
Denis Yoshchyk <bycter@gmail.com>
2017-05-18 15:30:16 +0300

    core.task16.task1623 processing Рекурсивное создание нитей

--- 2.JavaCore/src/com/javarush/task/task16/task1623/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task16/task1623/Solution.java
@@ -19,17 +19,33 @@ package com.javarush.task.task16.task1623;
 */
 
 public class Solution {
+
     static int count = 15;
-    static volatile int countCreatedThreads;
+    static volatile int countCreatedThreads = 0;
 
     public static void main(String[] args) {
+
         System.out.println(new GenerateThread());
     }
 
     public static class GenerateThread extends Thread {
 
         public GenerateThread() {
+
             super(Integer.toString(++countCreatedThreads));
+            this.start();
+        }
+
+        @Override
+        public void run() {
+
+            if (countCreatedThreads < count) System.out.println(new GenerateThread());
+        }
+
+        @Override
+        public String toString() {
+
+            return getName() + " created";
         }
     }
 }
