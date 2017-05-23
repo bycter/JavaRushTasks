2135a863a6df655bd07440b2af08b2819edbf195
Denis Yoshchyk <bycter@gmail.com>
2017-05-23 15:31:38 +0300

    core.task16.task1628 completed Кто первый встал - того и тапки

--- 2.JavaCore/src/com/javarush/task/task16/task1628/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task16/task1628/Solution.java
@@ -7,12 +7,24 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 
+/*
+Кто первый встал - того и тапки
+1. Разберись, что делает программа.
+1.1. Каждая нить должна читать с консоли слова. Используй готовый static BufferedReader reader.
+1.2. Используй AtomicInteger countReadStrings, чтобы посчитать, сколько слов уже считано с консоли всеми нитями.
+2. Реализуй логику метода run:
+2.1. Пока нить не прервана (!isInterrupted) читай с консоли слова и добавляй их в поле List<String> result.
+2.2. Используй countReadStrings для подсчета уже считанных с консоли слов.
+ */
+
 public class Solution {
+
     public static volatile AtomicInteger countReadStrings = new AtomicInteger(0);
     public static volatile BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
     public static void main(String[] args) throws IOException {
-        //read count of strings
+
+//        read count of strings
         int count = Integer.parseInt(reader.readLine());
 
         //init threads
@@ -38,14 +50,26 @@ public class Solution {
     }
 
     public static class ReaderThread extends Thread {
+
         private List<String> result = new ArrayList<String>();
 
+        @Override
         public void run() {
-            //add your code here - добавьте код тут
+
+            while (!isInterrupted()) {
+                try {
+                    if (reader.ready()) {
+                        result.add(reader.readLine());
+                        Solution.countReadStrings.incrementAndGet();
+                    }
+                } catch (IOException e) {
+                }
+            }
         }
 
         @Override
         public String toString() {
+
             return result.toString();
         }
     }
