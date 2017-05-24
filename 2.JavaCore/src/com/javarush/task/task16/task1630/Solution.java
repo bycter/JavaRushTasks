bf3e05d8594df2635ffe5859c9cde9f1f4d57e31
Denis Yoshchyk <bycter@gmail.com>
2017-05-24 21:00:49 +0300

    core.task16.task1630 completed Последовательный вывод файлов

--- 2.JavaCore/src/com/javarush/task/task16/task1630/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task16/task1630/Solution.java
@@ -1,26 +1,53 @@
 package com.javarush.task.task16.task1630;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+/*
+Последовательный вывод файлов
+1. Разберись, что делает программа.
+2. В статическом блоке считай 2 имени файла firstFileName и secondFileName.
+3. Внутри класса Solution создай нить public static ReadFileThread, которая реализует
+интерфейс ReadFileInterface (Подумай, что больше подходит — Thread или Runnable).
+3.1. Метод setFileName должен устанавливать имя файла, из которого будет читаться содержимое.
+3.2. Метод getFileContent должен возвращать содержимое файла.
+3.3. В методе run считай содержимое файла, закрой поток. Раздели пробелом строки файла.
+4. Подумай, в каком месте нужно подождать окончания работы нити, чтобы обеспечить последовательный вывод файлов.
+4.1. Для этого добавь вызов соответствующего метода.
+
+Ожидаемый вывод:
+[все тело первого файла]
+[все тело второго файла]
+
+
+ */
 
 public class Solution {
+
     public static String firstFileName;
     public static String secondFileName;
 
-    //add your code here - добавьте код тут
+    static {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        try {
+            firstFileName = reader.readLine();
+            secondFileName = reader.readLine();
+            reader.close();
+        } catch (IOException e) {
+        }
+    }
 
     public static void main(String[] args) throws InterruptedException {
+
         systemOutPrintln(firstFileName);
         systemOutPrintln(secondFileName);
     }
 
     public static void systemOutPrintln(String fileName) throws InterruptedException {
+
         ReadFileInterface f = new ReadFileThread();
         f.setFileName(fileName);
         f.start();
-        //add your code here - добавьте код тут
+        f.join();
         System.out.println(f.getFileContent());
     }
 
@@ -35,5 +62,44 @@ public class Solution {
         void start();
     }
 
-    //add your code here - добавьте код тут
+    public static class ReadFileThread extends Thread implements ReadFileInterface {
+
+        private String fileName;
+        StringBuffer stringBuffer = new StringBuffer();
+
+        @Override
+        public void setFileName(String fullFileName) {
+            this.fileName = fullFileName;
+        }
+
+        @Override
+        public String getFileContent() {
+
+            return stringBuffer.toString();
+        }
+
+        @Override
+        public void start() {
+            super.start();
+        }
+
+        @Override
+        public void run() {
+            BufferedReader fileReader = null;
+            try {
+                fileReader = new BufferedReader(new FileReader(fileName));
+            } catch (FileNotFoundException e) {
+                System.out.println("Flie " + fileName + " not found!");
+            }
+            try {
+                while (fileReader.ready()){
+                    stringBuffer.append(fileReader.readLine());
+                    stringBuffer.append(" ");
+                }
+                fileReader.close();
+            } catch (IOException e) {
+                System.out.println("IOException");
+            }
+        }
+    }
 }
