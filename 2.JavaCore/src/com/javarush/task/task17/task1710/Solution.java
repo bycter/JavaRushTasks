00b22ab46e05d806b1bdec031e0a828d9ca1b5bc
bycter <bycter@gmail.com>
2017-11-24 00:20:04 +0300

    core.task17.task1710 completed CRUD

--- 2.JavaCore/src/com/javarush/task/task17/task1710/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task17/task1710/Solution.java
@@ -1,8 +1,11 @@
 package com.javarush.task.task17.task1710;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 /* 
 CRUD
@@ -14,6 +17,7 @@ id соответствует индексу в списке
 */
 
 public class Solution {
+
     public static List<Person> allPeople = new ArrayList<Person>();
 
     static {
@@ -21,39 +25,89 @@ public class Solution {
         allPeople.add(Person.createMale("Петров Петр", new Date()));  //сегодня родился    id=1
     }
 
-    public static void main(String[] args) {
-        for (int i = 0; i < args.length; i++) {
-            System.out.println(i + " " + args[i]);
-        }
+    public static void main(String[] args) throws ParseException {
 
-        switch (args[0]){
-            case "-c":
-                addInfoPerson();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MMM-yyyy", Locale.ENGLISH);
+
+        Person person;
+        Date dateOfBirth;
+
+        switch (args[0]) {
+            case "-c": // Create Person
+                if (args[2] == "м") {
+                    allPeople.add(Person.createMale(args[1], dateFormat.parse(args[1])));
+                }
+                if (args[2] == "ж") {
+                    allPeople.add(Person.createFemale(args[1], dateFormat.parse(args[1])));
+                }
+                System.out.println(allPeople.size());
                 break;
             case "-u":
-                updateInfoPerson();
+                person = allPeople.get(Integer.parseInt(args[1]));
+                if (!person.getName().equals(args[2])){
+                    person.setName(args[2]);
+                }
+
+                if (args[3] == "м" && !person.getSex().equals(Sex.MALE)) person.setSex(Sex.MALE);
+                if (args[3] == "ж" && !person.getSex().equals(Sex.FEMALE)) person.setSex(Sex.FEMALE);
+
+                dateOfBirth =  dateFormat.parse(args[4]);
+                if (!person.getBirthDay().equals(dateOfBirth)){
+                    person.setBirthDay(dateOfBirth);
+                }
                 break;
             case "-d":
-                deleteInfoPerson();
+                person = allPeople.get(Integer.parseInt(args[1]));
+                person.setName(null);
+                person.setSex(null);
+                person.setBirthDay(null);
+                allPeople.add(Integer.parseInt(args[1]), person);
                 break;
             case "-i":
-                showInfoPerson();
+                StringBuilder stringBuilder = new StringBuilder();
+
+                person = allPeople.get(Integer.parseInt(args[1]));
+
+                stringBuilder.append(person.getName());
+                stringBuilder.append(" ");
+
+                if (person.getSex() == Sex.MALE) stringBuilder.append("м");
+                else stringBuilder.append("ж");
+                stringBuilder.append(" ");
+
+                stringBuilder.append(person.getBirthDay().toString());
                 break;
-
-
+// Миронов м 15-Apr-1990
         }
     }
+/*
+Программа запускается с одним из следующих наборов параметров:
+-c name sex bd
+-u id name sex bd
+-d id
+-i id
 
-    private static void addInfoPerson(){
+Значения параметров:
+name — имя, String
+sex — пол, «м» или «ж», одна буква
+bd — дата рождения в следующем формате 15/04/1990
+-c — добавляет человека с заданными параметрами в конец allPeople, выводит id (index) на экран
+-u — обновляет данные человека с данным id
+-d — производит логическое удаление человека с id, заменяет все его данные на null
+-i — выводит на экран информацию о человеке с id: name sex (м/ж) bd (формат 15-Apr-1990)
+id соответствует индексу в списке
+*/
+
+
+    private static void updateInfoPerson() {
 
     }
-    private static void updateInfoPerson(){
+
+    private static void deleteInfoPerson() {
 
     }
-    private static void deleteInfoPerson(){
 
-    }
-    private static void showInfoPerson(){
+    private static void showInfoPerson() {
 
     }
 }
